5927a68db4e6ef60223cd3e4994e38c1f59f9a65
adele <adele.r87@gmail.com>
2021-01-13 14:19:42 +0100

    slide next ok

---
+++ main.js
@@ -0,0 +1,36 @@
+
+// inseriamo tutto nel document
+
+$(document).ready(function () {
+
+  // su next clicco
+  $('.next').click(function(){
+    $('.slider-wrapper .images img.active').removeClass('active').next().addClass('active');
+  })
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+//
+// $(document).ready(function() {
+//
+//   $('.button-next').click(function() {
+//     $('.slider-box.active').removeClass('active').next().addClass('active');
+//   })
+//
+//   $('.button-prev').click(function() {
+//     $('.slider-box.active').removeClass('active').prev().addClass('active');
+//   })
+//
+// })
