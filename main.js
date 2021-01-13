e8d7f54b6148a0637937c0f61b967c021df86341
adele <adele.r87@gmail.com>
2021-01-13 16:01:18 +0100

    modifiche

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
 
 // inseriamo tutto nel document
 
+// e poi gli dico che
+// se l'immagine attiva ha la classe last, al click su next, aggiungi l'active all'immagine con classe first
+var imgLast = ('.slider-wrapper .images img.active.last')
+var imgFirst =('.slider-wrapper .images img.first')
+//
+
+if (imgLast) {
+  img.First.addClass('active');
+}
+
+
+
+
 $(document).ready(function () {
 
   // su next clicco
+
   $('.next').click(function(){
-    $('.slider-wrapper .images img.active').removeClass('active').next().addClass('active');
+
+      $('.slider-wrapper .images img.active').removeClass('active').next().addClass('active');
+
   })
 
+  // sul prev clicco
   $('.prev').click(function(){
     $('.slider-wrapper .images img.active').removeClass('active').next().addClass('active')
   })
 
-
 })
+
+
 
 
 
