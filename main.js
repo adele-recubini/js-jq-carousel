a9b9dbb667f29b40fddc9f8442b41309ca84888c
adele <adele.r87@gmail.com>
2021-01-14 11:35:43 +0100

    creazione delle funzioni generiche richiamate all interno del document

--- main.js
+++ main.js
@@ -3,81 +3,75 @@
 
 // e poi gli dico che
 // se l'immagine attiva ha la classe last, al click su next, aggiungi l'active all'immagine con classe first
+// faccio la stessa identica cosa con i pallini il discorso è lo stesso
+// creo due funzioni che richiamo all interno di document
+
+// FUNZIONE DEL NEXT
+
+function next() {
+  var activeImg = $('.images img.active');
+  var activeI = $('i.active');
+
+   activeImg.removeClass('active');
+   activeI.removeClass('active');
+
+    // se l immagine active ha anche la classe last
+
+  if(activeImg.hasClass('last') && activeI.hasClass('last')){
+    var firstImg =$('.images img.first');
+    var firstI =$('i.first');
+    // inserisco l active nella immagine con la classe first
+    firstImg.addClass('active')
+    firstI.addClass('active')
+
+  } else {
+   activeImg.next().addClass('active');
+   activeI.next().addClass('active');
+   }
+   }
+
+   // FUNZIONE DEL PREV
+   function prev() {
+     // creo variabile dell immagine con la classe active
+     var activeImg = $('.images img.active')
+     var activeI = $('i.active')
+     // poi la rimuovo
+     activeImg.removeClass('active')
+     activeI.removeClass('active')
 
 
+     // inizio l if
+     if (activeImg.hasClass('first') && activeI.hasClass('first')) {
+       var lastImg =$('.images img.last')
+       var lastI =$('i.last')
+
+       // inserisco l active nell immgine con la classe last
+       lastImg.addClass('active')
+       lastI.addClass('active')
+     }else {
+       activeImg.prev().addClass('active')
+       activeI.prev().addClass('active')
+     }
+
+   }
+
+
+   // le funzioni le richiamo allinterno del ready
 
 $(document).ready(function () {
 
-  // su next clicco
+  // su next clicco richiamo la funzione
 
-  $('.next').click(function(){
-    // ho creato var dell immagine con la classe active
- var activeImg = $('.images img.active');
-  activeImg.removeClass('active');
-
-   // se l immagine active ha anche la classe last
-
- if(activeImg.hasClass('last')){
-   var firstImg =$('.images img.first');
-   // inserisco l active nella immagine con la classe first
-   firstImg.addClass('active')
- } else {
-  activeImg.next().addClass('active');
-  }
+    $('.next').click(function(){
+    next();
   })
 
-
-  // sul prev clicco
+  // sul prev clicco e richiamo la funzione
 
   $('.prev').click(function(){
-    // creo variabile dell immagine con la classe active
-    var activeImg = $('.images img.active')
-    // poi la rimuovo
-    activeImg.removeClass('active')
+    prev();
 
-    // inizio l if
-    if (activeImg.hasClass('first')) {
-      var lastImg =$('.images img.last')
-      // inserisco l active nell immgine con la classe last
-      lastImg.addClass('active')
-    }else {
-      activeImg.prev().addClass('active')
-    }
-   })
-
-   // stessa cosa per i pallini
-
-   $('.nav>i').click(function(){
-
-     // assegno una variabile al pallino colorato cioe in active e poi gli rimuovo la classe dal primo a quarto
-
-     var activeCircle =$('.nav >i.active')
-     activeCircle.removeClass('active')
-
-     if (activeCircle.hasClass('last')) {
-        var firstCircle = $('.nav >i.first')
-        firstCircle.addClass('active')
-      }
-      else {
-       activeCircle.next().addClass('active')
-      }
-     })
-
-      // var activeCircle =$('.nav >i.active')
-      // activeCircle.removeClass('active')
-      //
-      //
-      // if (activeCircle.hasClass('first')) {
-      //   var lastCircle =  $('.nav >i.last')
-      //   lastCircle.addClass('active')
-      // }
-      // else {
-      //  activeCircle.next().addClass('active')
-      // }
-      //
-
-
-
+  })
 
 
     // fine document
